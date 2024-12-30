2067126892b00dfd3fa0825f0f3f6a73eaf46c35
abdulloh-id <outergamer11@gmail.com>
2024-12-30 23:42:01 +0500

    Added password reset feature via email

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,21 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# Showing email on the console
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+env = environ.Env()
+environ.Env.read_env()
+
+# Using real email service
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'outergamer11@gmail.com'  # Replace with your Gmail address
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'outergamer11@gmail.com'  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')  # Use an app-specific password
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
